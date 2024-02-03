78cf6543d0ad34c0545dc9389cb59042aa08b060
Kevin <johnthanselvadurai@gmail.com>
2024-02-02 19:55:11 -0500

    Concurrency
    Threading and Concurrency added to Main Class

--- src/main/java/com/playDB/PlayCommandLine.java
+++ src/main/java/com/playDB/PlayCommandLine.java
@@ -1,32 +1,55 @@
 package com.playDB;
 
 import java.util.Scanner;
-import java.util.concurrent.CountDownLatch; 
-
+import java.util.concurrent.CountDownLatch;
 
 public class PlayCommandLine {
+   public static String command = "";
+
 
     public static void main(String[] args) {
-        try (Scanner myObj = new Scanner(System.in)) {
-            CommandInvoker invoker = CommandInvoker.getInstance();
-            String command = "";
+            //String command = "";
+
+                //System.out.print("PlayDB> ");
+                //command = myObj.nextLine();
+
+                    // Start a new thread
+            Thread newThread = new Thread(() -> {
+                    
+                  try (Scanner myObj = new Scanner(System.in)) {
+                    	
+                        CommandInvoker invoker = CommandInvoker.getInstance();
+
+                        while (!command.equalsIgnoreCase("EXIT")) {
+
+	                    
+                            try {
+                        	   System.out.print("PlayDB> ");
+	                           command = myObj.nextLine();
+	                           invoker.pushCommand(command);
+	                           invoker.evoke();
+                        	  }
+                        	catch(Exception e) {
+                        		System.out.println(e);
+                        	}
+                        
+                        
+                        
+                        
+                        }
+                          System.out.println("Goodbye :) ");
+                    }  catch (Exception e) {
+                               e.printStackTrace();
+                        }
+                     
+               });
+
+                
+                newThread.start();
+
+               // invoker.pushCommand(command);
+                //invoker.evoke();
             
-          
 
-            while (!command.equalsIgnoreCase("EXIT")) {
-                System.out.print("PlayDB> ");
-                command = myObj.nextLine();
-
-                invoker.pushCommand(command);
-                invoker.evoke();
-                
-                
-            }
-
-            System.out.println("Goodbye :) ");
-        } catch (Exception e) {
-            e.printStackTrace();
-        }
     }
 }
-
